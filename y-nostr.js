97d3b6da4c27d2d45b63e40ed84705335f0f1a0f
Pirx <82919676+pirxthedev@users.noreply.github.com>
2023-02-28 21:08:44 -0500

    Add url and room name to constructor args

--- y-nostr.js
+++ y-nostr.js
@@ -1,8 +1,10 @@
 import { Observable } from 'lib0/observable'
 
 export class NostrProvider extends Observable {
-    constructor(doc) {
+    constructor(relayUrl, roomName, doc) {
         super()
+        this.relayUrl = relayUrl
+        this.roomName = roomName
         this.doc = doc
     }
 }
--- y-nostr.test.js
+++ y-nostr.test.js
@@ -7,8 +7,10 @@ test('provider object is an Observable', () => {
     expect(new NostrProvider()).toBeInstanceOf(Observable)
 });
 
-test('can register yjs doc to provider', () => {
+test('can initialize provider with required configuration info', () => {
     const doc = new Y.Doc()
-    const provider = new NostrProvider(doc)
+    const provider = new NostrProvider('wss://127.0.0.1:8008/', 'test-room', doc)
+    expect(provider.relayUrl).toBe('wss://127.0.0.1:8008/')
+    expect(provider.roomName).toBe('test-room')
     expect(provider.doc).toBe(doc)
 });
