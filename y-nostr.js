48c0f281fd80b80228a0f7e43b52f881162d67da
Pirx <82919676+pirxthedev@users.noreply.github.com>
2023-02-28 21:01:20 -0500

    register yjs doc to provider

--- y-nostr.js
+++ y-nostr.js
@@ -1,5 +1,8 @@
 import { Observable } from 'lib0/observable'
 
 export class NostrProvider extends Observable {
-
+    constructor(doc) {
+        super()
+        this.doc = doc
+    }
 }
--- y-nostr.test.js
+++ y-nostr.test.js
@@ -1,6 +1,14 @@
 import { NostrProvider } from 'y-nostr'
 import { Observable } from 'lib0/observable'
+import * as Y from 'yjs'
+
 
 test('provider object is an Observable', () => {
     expect(new NostrProvider()).toBeInstanceOf(Observable)
+});
+
+test('can register yjs doc to provider', () => {
+    const doc = new Y.Doc()
+    const provider = new NostrProvider(doc)
+    expect(provider.doc).toBe(doc)
 });
