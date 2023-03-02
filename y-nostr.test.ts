088621c8b7cbc469e7c409ce64b4ce3d4b6200f5
Pirx <82919676+pirxthedev@users.noreply.github.com>
2023-03-01 22:29:00 -0500

    publish update events to nostr

--- y-nostr.test.ts
+++ y-nostr.test.ts
@@ -2,6 +2,7 @@ import { NostrProvider } from './y-nostr'
 import { Observable } from 'lib0/observable'
 import * as Y from 'yjs'
 require('websocket-polyfill')
+import { fromUint8Array } from 'js-base64'
 
 let doc: Y.Doc;
 let provider: NostrProvider;
@@ -46,4 +47,27 @@ test('connect command with available relay causes success', done => {
     })
     provider.relay.connect = jest.fn(() => Promise.resolve())
     provider.connect()
+})
+
+test('publish update event to nostr if doc is updated by the provider', () => {
+    const publishMock = jest.fn()
+    provider.relay.publish = publishMock
+    doc.transact(() => {
+        doc.getText('test-room').insert(0, 'Hello')
+    })
+    expect(provider.relay.publish).toHaveBeenCalled()
+})
+
+test('content field of update event has recommended yjs protocol encoding', () => {
+    const publishMock = jest.fn()
+    provider.relay.publish = publishMock
+    doc.transact(() => {
+        doc.getText('test-room').insert(0, 'Hello')
+    })
+    const documentState = Y.encodeStateAsUpdate(doc)
+    const base64Encoded = fromUint8Array(documentState)
+    expect(publishMock.mock.calls[0][0]).toHaveProperty(
+        'content',
+        base64Encoded
+    )
 })
--- y-nostr.ts
+++ y-nostr.ts
@@ -1,7 +1,9 @@
 import { Observable } from 'lib0/observable'
-import { relayInit } from 'nostr-tools'
+import { getEventHash, relayInit } from 'nostr-tools'
 import * as Y from 'yjs'
-import type { Relay } from 'nostr-tools'
+import type { Relay, UnsignedEvent, Event } from 'nostr-tools'
+import { generatePrivateKey, getPublicKey, signEvent } from 'nostr-tools'
+import { fromUint8Array } from 'js-base64'
 
 export class NostrProvider extends Observable<any> {
     relayUrl: string
@@ -15,6 +17,24 @@ export class NostrProvider extends Observable<any> {
         this.roomName = roomName
         this.doc = doc
         this.relay = relayInit(this.relayUrl)
+
+        this.doc.on('update', (update: any) => {
+            let sk = generatePrivateKey()
+            let pk = getPublicKey(sk)
+            let unsignedEvent: UnsignedEvent = {
+                kind: 1,
+                content: fromUint8Array(update),
+                tags: [],
+                created_at: Math.floor(Date.now() / 1000),
+                pubkey: pk,
+            }
+            let event: Event = {
+                ...unsignedEvent,
+                id: getEventHash(unsignedEvent),
+                sig: signEvent(unsignedEvent, sk)
+            }
+            this.relay.publish(event)
+        })
 
     }
 
